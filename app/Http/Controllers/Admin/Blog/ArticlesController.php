683ea6b69c68686cad2b0a398f734003c26a7727
Radoslav Radev <radev.n.radoslav@gmail.com>
2022-09-10 11:29:20 +0300

    Blog

--- app/Http/Controllers/Admin/Blog/ArticlesController.php
+++ app/Http/Controllers/Admin/Blog/ArticlesController.php
@@ -3,9 +3,80 @@
 namespace App\Http\Controllers\Admin\Blog;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\Blog\Articles\DeleteRequest;
+use App\Http\Requests\Admin\Blog\Articles\DetailsRequest;
+use App\Http\Requests\Admin\Blog\Articles\IndexRequest;
+use App\Http\Requests\Admin\Blog\Articles\StoreRequest;
+use App\Http\Requests\Admin\Blog\Articles\UpdateRequest;
+use App\Models\BlogArticle;
 
 class ArticlesController extends Controller
 {
-    //
+    /**
+     * Get all articles
+     */
+    public function index(IndexRequest $request)
+    {
+        $articles = BlogArticle::orderBy('created_at', 'DESC')
+            ->get();
+        
+        return response([
+            'data' => $articles
+        ], 200);
+    }
+
+    /**
+     * Get details about an article
+     */
+    public function details($id, DetailsRequest $request)
+    {
+        $article = BlogArticle::findOrFail($id);
+
+        return response([
+            'data' => $article
+        ], 200);
+    }
+
+    /**
+     * Store an article
+     */
+    public function store($id, StoreRequest $request)
+    {
+        $data = $request->toArray();
+        $data['category_id'] = $id;
+
+        $article = BlogArticle::create($data);
+
+        return response([
+            'data' => $article
+        ], 200);
+    }
+    
+    /**
+     * Update an article
+     */
+    public function update($id, UpdateRequest $request)
+    {
+        $article = BlogArticle::findOrFail($id);
+
+        $article->update($request->toArray());
+
+        return response([
+            
+        ], 200);
+    }
+
+    /**
+     * Delete an article
+     */
+    public function delete($id, DeleteRequest $request)
+    {
+        $article = BlogArticle::findOrFail($id);
+
+        $article->delete();
+
+        return response([
+            
+        ], 200);
+    }
 }
--- app/Http/Controllers/Admin/Blog/CategoriesController.php
+++ app/Http/Controllers/Admin/Blog/CategoriesController.php
@@ -3,9 +3,80 @@
 namespace App\Http\Controllers\Admin\Blog;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\Blog\Categories\DeleteRequest;
+use App\Http\Requests\Admin\Blog\Categories\DetailsRequest;
+use App\Http\Requests\Admin\Blog\Categories\IndexRequest;
+use App\Http\Requests\Admin\Blog\Categories\StoreRequest;
+use App\Http\Requests\Admin\Blog\Categories\UpdateRequest;
+use App\Models\BlogCategory;
 
 class CategoriesController extends Controller
 {
-    //
+    /**
+     * Get all categories
+     */
+    public function index(IndexRequest $request)
+    {
+        $categories = BlogCategory::orderBy('created_at', 'DESC')
+            ->get();
+        
+        return response([
+            'data' => $categories
+        ], 200);
+    }
+
+    /**
+     * Get details about an category
+     */
+    public function details($id, DetailsRequest $request)
+    {
+        $category = BlogCategory::findOrFail($id);
+
+        return response([
+            'data' => $category
+        ], 200);
+    }
+
+    /**
+     * Store an category
+     */
+    public function store($id, StoreRequest $request)
+    {
+        $data = $request->toArray();
+        $data['category_id'] = $id;
+
+        $category = BlogCategory::create($data);
+
+        return response([
+            'data' => $category
+        ], 200);
+    }
+    
+    /**
+     * Update an category
+     */
+    public function update($id, UpdateRequest $request)
+    {
+        $category = BlogCategory::findOrFail($id);
+
+        $category->update($request->toArray());
+
+        return response([
+            
+        ], 200);
+    }
+
+    /**
+     * Delete an category
+     */
+    public function delete($id, DeleteRequest $request)
+    {
+        $category = BlogCategory::findOrFail($id);
+
+        $category->delete();
+
+        return response([
+            
+        ], 200);
+    }
 }
